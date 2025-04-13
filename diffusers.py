f5e3d8bf5e84e23500ea7efaccb53d12b88ecc6c
Disty0 <disty@disty.xyz>
2025-04-14 00:51:45 +0300

    Fix HiDream

--- diffusers.py
+++ diffusers.py
@@ -42,9 +42,33 @@ class FluxPosEmbed(torch.nn.Module):
         return freqs_cos, freqs_sin
 
 
+def hidream_rope(pos: torch.Tensor, dim: int, theta: int) -> torch.Tensor:
+    assert dim % 2 == 0, "The dimension must be even."
+    return_device = pos.device
+    pos = pos.to("cpu")
+
+    scale = torch.arange(0, dim, 2, dtype=torch.float64, device=pos.device) / dim
+    omega = 1.0 / (theta**scale)
+
+    batch_size, seq_length = pos.shape
+    out = torch.einsum("...n,d->...nd", pos, omega)
+    cos_out = torch.cos(out)
+    sin_out = torch.sin(out)
+
+    stacked_out = torch.stack([cos_out, -sin_out, sin_out, cos_out], dim=-1)
+    out = stacked_out.view(batch_size, -1, dim // 2, 2, 2)
+    return out.to(return_device, dtype=torch.float32)
+
+
 def ipex_diffusers(device_supports_fp64=False, can_allocate_plus_4gb=False):
+    # get around lazy imports
+    from diffusers.utils import torch_utils # pylint: disable=import-error, unused-import # noqa: F401
     diffusers.utils.torch_utils.fourier_filter = fourier_filter
     if not device_supports_fp64:
+        # get around lazy imports
+        from diffusers.models import transformers as diffusers_transformers # pylint: disable=import-error, unused-import # noqa: F401
+        from diffusers.models import controlnets as diffusers_controlnets # pylint: disable=import-error, unused-import # noqa: F401
         diffusers.models.embeddings.FluxPosEmbed = FluxPosEmbed
         diffusers.models.transformers.transformer_flux.FluxPosEmbed = FluxPosEmbed
         diffusers.models.controlnets.controlnet_flux.FluxPosEmbed = FluxPosEmbed
+        diffusers.models.transformers.transformer_hidream_image.rope = hidream_rope
